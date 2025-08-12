6fdf62300d8059d0950ec4584fc69893b6e746f3
Cellery Lin <yochen0123@gmail.com>
2025-08-12 17:15:38 +0800

    add api url

--- client-website/src/components/ApiUrl.tsx
+++ client-website/src/components/ApiUrl.tsx
@@ -1,3 +1,3 @@
-const SurvivalAnalysisAPI = 'http://140.117.60.136:5000/api/survival_analysis'
+const SurvivalAnalysisAPI = 'https://shrimp-improved-frequently.ngrok-free.app/api/survival_analysis'
 
 export { SurvivalAnalysisAPI }
